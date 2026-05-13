19c3569cddb49c191de38cb556823f9cfbfee5a6
saurabhchandanshive01 <saurabhchandanshive01@gmail.com>
2026-05-13 18:25:22 +0530

    cookies properties updated for production

--- server/server.ts
+++ server/server.ts
@@ -24,20 +24,32 @@ await connectDB();
 const app = express();
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:3000',"https://thumblify-lilac-five.vercel.app/"],
+    origin: ['http://localhost:5173', 'http://localhost:3000', "https://thumblify-lilac-five.vercel.app/"],
     credentials: true
 }))
 
-app.use(session({
-    secret: process.env.SESSION_SECRET as string,
-    resave: false,
-    saveUninitialized: false,
-    cookie: { maxAge: 1000 * 60 * 60 * 24 * 7 }, // 7 days
-    store: MongoStore.create({
-        mongoUrl: process.env.MONGODB_URI as string,
-        collectionName: 'sessions'
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET as string,
+
+        resave: false,
+
+        saveUninitialized: false,
+
+        cookie: {
+            maxAge: 1000 * 60 * 60 * 24 * 7,
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "none",
+            path: "/",
+        },
+
+        store: MongoStore.create({
+            mongoUrl: process.env.MONGODB_URI as string,
+            collectionName: "sessions",
+        }),
     })
-}))
+);
 app.use(express.json());
 
 app.get(
