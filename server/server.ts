cb3c6c205a94c5565b857001a849edb105334f0f
saurabhchandanshive01 <saurabhchandanshive01@gmail.com>
2026-05-13 18:22:22 +0530

    url updated in cors

--- server/server.ts
+++ server/server.ts
@@ -24,7 +24,7 @@ await connectDB();
 const app = express();
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    origin: ['http://localhost:5173', 'http://localhost:3000',"https://thumblify-lilac-five.vercel.app/"],
     credentials: true
 }))
 
